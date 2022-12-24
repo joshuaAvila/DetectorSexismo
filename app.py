5189d6b38ef7e22a70839cd6a554bca77ff41c63
joshuaAvila <69708499+joshuaAvila@users.noreply.github.com>
2022-12-24 09:54:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import streamlit as st
 
+
+def highlight_survived(s):
+    return ['background-color: red']*len(s) if (s.Sexista == 1) else ['background-color: green']*len(s)
+
+def color_survived(val):
+    color = 'red' if val=='Sexista' else 'white'
+    return f'background-color: {color}'                
+
+st.set_page_config(layout="wide")
+st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
+
+#background-color: Blue;
+
+colT1,colT2 = st.columns([2,8])
+with colT2:
+    #st.title('Analisis de comentarios sexistas en Twitter') 
+    st.markdown(""" <style> .font {
+    font-size:40px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Análisis de comentarios sexistas en Twitter</p>', unsafe_allow_html=True)
+    
+    st.markdown(""" <style> .font1 {
+    font-size:28px ; font-family: 'Times New Roman'; color: #8d33ff;} 
+    </style> """, unsafe_allow_html=True)
+
+    st.markdown(""" <style> .font2 {
+    font-size:16px ; font-family: 'Times New Roman'; color: #3358ff;} 
+    </style> """, unsafe_allow_html=True)
