d0cc64b2367c796c0840fba24b0c48d671c8a9c1
joshuaAvila <69708499+joshuaAvila@users.noreply.github.com>
2022-12-26 14:36:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import regex as re
+import re
 
 def highlight_survived(s):
     return ['background-color: red']*len(s) if (s.Sexista == 1) else ['background-color: green']*len(s)
