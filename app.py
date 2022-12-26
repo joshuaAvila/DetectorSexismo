4b3dcacaf1a7d100682a1cf0e527b70733d10930
joshuaAvila <69708499+joshuaAvila@users.noreply.github.com>
2022-12-26 17:49:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ import pandas as pd
 import numpy as np
 import re
 
+from pysentimiento.preprocessing import preprocess_tweet
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import AutoTokenizer, AutoModelForSequenceClassification,AdamW
+tokenizer = AutoTokenizer.from_pretrained('hackathon-pln-es/twitter_sexismo-finetuned-robertuito-exist2021')
+model = AutoModelForSequenceClassification.from_pretrained("hackathon-pln-es/twitter_sexismo-finetuned-robertuito-exist2021")
+
+import torch
+if torch.cuda.is_available():  
+    device = torch.device(	"cuda")
+    print('I will use the GPU:', torch.cuda.get_device_name(0))
+    
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+    
 def highlight_survived(s):
     return ['background-color: red']*len(s) if (s.Sexista == 1) else ['background-color: green']*len(s)
 
