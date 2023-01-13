f92c87dcd6242c2c70a71d48e2a1076fc946213e
joshuaAvila <69708499+joshuaAvila@users.noreply.github.com>
2023-01-12 22:15:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import regex as re
 import torch
-
+import pysentimiento
 
 
 from pysentimiento.preprocessing import preprocess_tweet
@@ -13,6 +13,15 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler, Sequentia
 from transformers import AutoTokenizer, AutoModelForSequenceClassification,AdamW
 tokenizer = AutoTokenizer.from_pretrained('hackathon-pln-es/twitter_sexismo-finetuned-robertuito-exist2021')
 model = AutoModelForSequenceClassification.from_pretrained("hackathon-pln-es/twitter_sexismo-finetuned-robertuito-exist2021")
+
+import torch
+if torch.cuda.is_available():  
+    device = torch.device(	"cuda")
+    print('I will use the GPU:', torch.cuda.get_device_name(0))
+    
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
 
     
 consumer_key = "BjipwQslVG4vBdy4qK318KnoA"
@@ -23,41 +32,12 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
-def preprocess(text):
-    text=text.lower()
-    # remove hyperlinks
-    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)
-    text = re.sub(r'http?:\/\/.*[\r\n]*', '', text)
-    #Replace &amp, &lt, &gt with &,<,> respectively
-    text=text.replace(r'&amp;?',r'and')
-    text=text.replace(r'&lt;',r'<')
-    text=text.replace(r'&gt;',r'>')
-    #remove hashtag sign
-    #text=re.sub(r"#","",text)   
-    #remove mentions
-    text = re.sub(r"(?:\@)\w+", '', text)
-    #text=re.sub(r"@","",text)
-    #remove non ascii chars
-    text=text.encode("ascii",errors="ignore").decode()
-    #remove some puncts (except . ! ?)
-    text=re.sub(r'[:"#$%&\*+,-/:;<=>@\\^_`{|}~]+','',text)
-    text=re.sub(r'[!]+','!',text)
-    text=re.sub(r'[?]+','?',text)
-    text=re.sub(r'[.]+','.',text)
-    text=re.sub(r"'","",text)
-    text=re.sub(r"\(","",text)
-    text=re.sub(r"\)","",text)
-    text=" ".join(text.split())
-    return text
-    
-    
-    
 def highlight_survived(s):
     return ['background-color: red']*len(s) if (s.Sexista == 1) else ['background-color: green']*len(s)
 
 def color_survived(val):
     color = 'red' if val=='Sexista' else 'white'
-    return f'background-color: {color}'                
+    return f'background-color: {color}'
 
 st.set_page_config(layout="wide")
 st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
@@ -80,32 +60,31 @@ with colT2:
 
    
 def run():   
-     with st.form("my_form"):
-       col,buff1, buff2 = st.columns([2,2,1])
-       st.write("Escoja una Opción")
-       search_words = col.text_input("Introduzca el termino o usuario para analizar y pulse el check correspondiente")
-       number_of_tweets = col.number_input('Introduzca número de twweets a analizar. Máximo 50', 0,50,10)
-       termino=st.checkbox('Término')
-       usuario=st.checkbox('Usuario')
-       submit_button = col.form_submit_button(label='Analizar')
-       error=False
-       if submit_button:
-        date_since = "2020-09-14"
-        if (termino == False and usuario == False):
-            st.text('Error no se ha seleccionado ningun check')
-            error=True
-        elif (termino == True and usuario == True):
-            st.text('Error se han seleccionado los dos check')
-            error=True
+ with st.form("my_form"):
+   col,buff1, buff2 = st.columns([2,2,1])
+   st.write("Escoja una Opción")
+   search_words = col.text_input("Introduzca el termino o usuario para analizar y pulse el check correspondiente")
+   number_of_tweets = col.number_input('Introduzca número de twweets a analizar. Máximo 50', 0,50,10)
+   termino=st.checkbox('Término')
+   usuario=st.checkbox('Usuario')
+   submit_button = col.form_submit_button(label='Analizar')
+   error=False
+   if submit_button:
+            date_since = "2020-09-14"
+            if ( termino == False and usuario == False):
+                st.text('Error no se ha seleccionado ningun check')
+                error=True
+            elif ( termino == True and usuario == True):
+                st.text('Error se han seleccionado los dos check')
+                error=True
                 
+            if (error == False):
+                if (termino):
+                    new_search = search_words + " -filter:retweets"
+                    tweets =tw.Cursor(api.search_tweets,q=new_search,lang="es",since=date_since).items(number_of_tweets)
+                elif (usuario):
+                    tweets = api.user_timeline(screen_name = search_words,count=number_of_tweets)
                 
-        if (error == False):
-            if (termino):
-                new_search = search_words + " -filter:retweets"
-                tweets =tw.Cursor(api.search_tweets,q=new_search,lang="es",since=date_since).items(number_of_tweets)
-            elif (usuario):
-                tweets = api.user_timeline(screen_name = search_words,count=number_of_tweets)
-
                 tweet_list = [i.text for i in tweets]
                 #tweet_list = [strip_undesired_chars(i.text) for i in tweets]
                 text= pd.DataFrame(tweet_list)
@@ -113,11 +92,11 @@ def run():
                 text[0] = text[0].apply(preprocess_tweet)
                 text1=text[0].values
                 indices1=tokenizer.batch_encode_plus(text1.tolist(),
-                                                     max_length=128,
-                                                     add_special_tokens=True, 
-                                                     return_attention_mask=True,
-                                                     pad_to_max_length=True,
-                                                     truncation=True)
+                                         max_length=128,
+                                         add_special_tokens=True, 
+                                         return_attention_mask=True,
+                                         pad_to_max_length=True,
+                                         truncation=True)
                 input_ids1=indices1["input_ids"]
                 attention_masks1=indices1["attention_mask"]
                 prediction_inputs1= torch.tensor(input_ids1)
@@ -140,8 +119,8 @@ def run():
                     b_input_ids1, b_input_mask1 = batch
                     # Telling the model not to compute or store gradients, saving memory and   # speeding up prediction
                     with torch.no_grad():
-                       # Forward pass, calculate logit predictions
-                       outputs1 = model(b_input_ids1, token_type_ids=None,attention_mask=b_input_mask1)
+                        # Forward pass, calculate logit predictions
+                        outputs1 = model(b_input_ids1, token_type_ids=None,attention_mask=b_input_mask1)
                     logits1 = outputs1[0]
                     # Move logits and labels to CPU
                     logits1 = logits1.detach().cpu().numpy()
@@ -151,10 +130,8 @@ def run():
                 flat_predictions = np.argmax(flat_predictions, axis=1).flatten()#p = [i for i in classifier(tweet_list)]
                 df = pd.DataFrame(list(zip(tweet_list, flat_predictions)),columns =['Últimos '+ str(number_of_tweets)+' Tweets'+' de '+search_words, 'Sexista'])
                 df['Sexista']= np.where(df['Sexista']== 0, 'No Sexista', 'Sexista')
-
-
+                
+                
                 st.table(df.reset_index(drop=True).head(20).style.applymap(color_survived, subset=['Sexista']))
-
-            
 
 run()
